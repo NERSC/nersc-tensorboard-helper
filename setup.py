232c8f8258114ca81b2d19cf951ffbc4ffabe001
Steve Farrell <sparticlesteve@gmail.com>
2021-03-11 13:33:54 -0800

    move from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='nersc-tensorboard-helper',
-      version='0.1',
-      url='https://github.com/NERSC/nersc-tensorboard-helper',
-      py_modules=['nersc_tensorboard_helper'])
+setuptools.setup(name='nersc-tensorboard-helper',
+                 version='0.1',
+                 url='https://github.com/NERSC/nersc-tensorboard-helper',
+                 py_modules=['nersc_tensorboard_helper'])
