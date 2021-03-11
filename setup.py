1fe23fc83e767a1de21367cc5724348fa8632eeb
Steve Farrell <sparticlesteve@gmail.com>
2021-03-11 10:40:08 -0800

    add very simple setup.py script

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='nersc-tensorboard-helper',
+      version='0.1',
+      url='https://github.com/NERSC/nersc-tensorboard-helper',
+      py_modules=['nersc_tensorboard_helper'])
